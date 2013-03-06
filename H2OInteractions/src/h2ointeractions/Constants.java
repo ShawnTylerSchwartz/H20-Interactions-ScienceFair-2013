0167597b876a138da0d3aff3735f750433d78ffa
Shawn Schwartz <waverkid97@gmail.com>
2013-03-05 17:09:44 -0800

    Adds base files.

---
+++ H2OInteractions/src/h2ointeractions/Constants.java
@@ -0,0 +1,13 @@
+package h2ointeractions;
+
+/**
+ * @author Shawn Tyler Schwartz
+ * @class Period 5, Honors Chemistry 10B
+ * @school High Tech Los Angeles
+ * @assistants Wun Chiou & Daniel Perahya
+ * @fileName "Constants.java" (Constants Class)
+ * @version 1.0 - 2013 LA County Science Fair Edition
+ */
+public class Constants {
+    
+}
---
+++ H2OInteractions/src/h2ointeractions/H2OInteractions.java
@@ -0,0 +1,16 @@
+package h2ointeractions;
+
+/**
+ * @author Shawn Tyler Schwartz
+ * @class Period 5, Honors Chemistry 10B
+ * @school High Tech Los Angeles
+ * @assistants Wun Chiou & Daniel Perahya
+ * @fileName "H2OInteractions.java" (Main Class)
+ * @version 1.0 - 2013 LA County Science Fair Edition
+ */
+public class H2OInteractions {
+
+    public static void main(String[] args) {
+
+    }
+}
