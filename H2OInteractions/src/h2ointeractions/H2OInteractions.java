d825be7960fa079d51540517f9ead296191732b7
Shawn Schwartz <shawn@sublimesynthesis.com>
2013-03-10 14:13:02 -0700

    Adds new arrays, their initializations, and the storing of their values into variables for calculation use. New "pointchargecalculation" method added as well.

--- H2OInteractions/src/h2ointeractions/H2OInteractions.java
+++ H2OInteractions/src/h2ointeractions/H2OInteractions.java
@@ -42,6 +42,12 @@ public class H2OInteractions {
     
     //Arrays
     public static double moleculePositions[];
+    public static double leftHydrogenOneCoord[];
+    public static double rightHydrogenOneCoord[];
+    public static double oxygenOneCoord[];
+    public static double oxygenTwoCoord[];
+    public static double leftHydrogenTwoCoord[];
+    public static double rightHydrogenTwoCoord[];
     public static String arrayContentstoPrint;
     
     //Variables for Calculations
@@ -81,10 +87,26 @@ public class H2OInteractions {
             //out.write("Test output");
             //out.write("Test: "+ Arrays.toString(moleculePositions));
             moleculePositions = new double[4];
+            leftHydrogenOneCoord = new double[1];
+            rightHydrogenOneCoord = new double[1];
+            oxygenOneCoord = new double[1];
+            oxygenTwoCoord = new double[1];
+            leftHydrogenTwoCoord = new double[1];
+            rightHydrogenTwoCoord = new double[1];
+            
             for(int n = 0; n < 4; n++) {
                 moleculePositions[n] = (double) (Math.random()*10);
                 arrayContentstoPrint = Arrays.toString(moleculePositions);
             //sopl(Arrays.toString(moleculePositions));             
+            }
+            
+            for(int i = 0; i < 2; i++) {
+                leftHydrogenOneCoord[i] = (double) (Math.random()*4);
+                rightHydrogenOneCoord[i] = (double) (Math.random()*4);
+                oxygenOneCoord[i] = (double) (Math.random()*4);
+                oxygenTwoCoord[i] = (double) (Math.random()*4);
+                leftHydrogenTwoCoord[i] = (double) (Math.random()*4);
+                rightHydrogenTwoCoord[i] = (double) (Math.random()*4);
             }
             
             sopl(arrayContentstoPrint);
@@ -98,24 +120,36 @@ public class H2OInteractions {
         }
         
         System.out.println(readFile("output.txt"));
-        for(int i = 0; i < 2; i++) {
-            x1 = moleculePositions[0];
-            y1 = moleculePositions[1];
-            x2 = moleculePositions[2];
-            y2 = moleculePositions[3];
-        }
-            System.out.println(x1);
-            System.out.println(y1);
-            System.out.println(x2);
-            System.out.println(y2);
-            System.out.println("\nPoint Charge Calculation: " + calculatePointCharge(2));
-            
-            for(int i = 1; i < 10; i++) {
-                distance = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
-                energy = ((Constants.kConstant*hydrogenCharge*oxygenCharge)/Math.abs(distance));
-                System.out.println(i+ " Test: "+ energy);
-                
-            }
+//        for(int i = 0; i < 2; i++) {
+//            x1 = moleculePositions[0];
+//            y1 = moleculePositions[1];
+//            x2 = moleculePositions[2];
+//            y2 = moleculePositions[3];
+//        }
+//            System.out.println(x1);
+//            System.out.println(y1);
+//            System.out.println(x2);
+//            System.out.println(y2);
+//            System.out.println("\nPoint Charge Calculation: " + calculatePointCharge(2));
+              leftHydrogenOnex1 = leftHydrogenOneCoord[0];
+              leftHydrogenOney1 = leftHydrogenOneCoord[1];
+              leftHydrogenTwox2 = leftHydrogenTwoCoord[0];
+              leftHydrogenTwoy2 = leftHydrogenTwoCoord[1];
+              oxygenOnex1 = oxygenOneCoord[0];
+              oxygenOney1 = oxygenOneCoord[1];
+              oxygenTwox2 = oxygenTwoCoord[0];
+              oxygenTwoy2 = oxygenTwoCoord[1];
+              rightHydrogenOnex1 = rightHydrogenOneCoord[0];
+              rightHydrogenOney1 = rightHydrogenOneCoord[1];
+              rightHydrogenTwox2 = rightHydrogenTwoCoord[0];
+              rightHydrogenTwoy2 = rightHydrogenTwoCoord[1];
+//              
+//            for(int i = 1; i < 10; i++) {
+//                distance = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+//                energy = ((Constants.kConstant*hydrogenCharge*oxygenCharge)/Math.abs(distance));
+//                System.out.println(i+ " Test: "+ energy);
+//                
+//            }
     }
    
     private static String readFile(String path) throws IOException {
@@ -148,20 +182,24 @@ public class H2OInteractions {
     }
     
     //Point (partial) charge calculation
-    public static double calculatePointCharge(double distance) {
-        for(int i = 1; i < 10; i++) {
-            distance = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
-            energy = ((Constants.kConstant*hydrogenCharge*oxygenCharge)/Math.abs(distance));
-            System.out.println(energy + i);
-        }
-//        energy += totalEnergy;
-//        return totalEnergy;
+//    public static double calculatePointCharge(double distance) {
+//        for(int i = 1; i < 10; i++) {
+//            distance = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+//            energy = ((Constants.kConstant*hydrogenCharge*oxygenCharge)/Math.abs(distance));
+//            System.out.println(energy + i);
+//        }
+////        energy += totalEnergy;
+////        return totalEnergy;
+//        return energy;
+//    }
+    
+    public static double calculatePointCharge(double moleculeOneX1, double moleculeTwoX2, double moleculeOneY1, double moleculeTwoY2, double distance) {
+        distance = Math.sqrt(Math.sqrt(Math.pow(moleculeTwoX2-moleculeOneX1, 2) + Math.pow(moleculeTwoY2-moleculeOneY1, 2)));
+        energy = Constants.kConstant*hydrogenCharge*oxygenCharge/Math.abs(distance);
+        System.out.println(energy);
         return energy;
     }
     
-    public static double calculatePointChargeHydrogens(double distance) {
-        distance = Math.sqrt(Math.pow())
-    }
 //    
 //    public static double calculateDistance(double distance) {
 //        for(int i = 1; i < 10; i++) {
