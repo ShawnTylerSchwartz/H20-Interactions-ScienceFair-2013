a75234f760d7780d22b301f19f0c4697427b4710
Shawn Schwartz <shawn@sublimesynthesis.com>
2013-03-10 17:37:38 -0700

    Array storing and printing and calculating now working. PROGRESS!

--- H2OInteractions/src/h2ointeractions/H2OInteractions.java
+++ H2OInteractions/src/h2ointeractions/H2OInteractions.java
@@ -41,7 +41,7 @@ public class H2OInteractions {
      */
     
     //Arrays
-    public static double moleculePositions[];
+//    public static double moleculePositions[];
     public static double leftHydrogenOneCoord[];
     public static double rightHydrogenOneCoord[];
     public static double oxygenOneCoord[];
@@ -78,28 +78,42 @@ public class H2OInteractions {
     public static double oxygenCharge = Constants.KoxygenPointCharge;
     
     public static void main(String[] args) throws IOException {
-//        Random moleculeRandom = new Random();
-//        int testVar = moleculeRandom.nextInt(50) + 1;
+            
+           
+            
+            
+            
+            
+        
+              System.out.println("leftHydrogenOnex1: " + leftHydrogenOnex1);
+              System.out.println("leftHydrogenOney1: " + leftHydrogenOney1);
+              System.out.println("leftHydrogenTwox2: " + leftHydrogenTwox2);
+              System.out.println("leftHydrogenTwoy2: " + leftHydrogenTwoy2);
+              System.out.println("oxygenOnex1: " + oxygenOnex1);
+              System.out.println("oxygenOney1: " + oxygenOney1);
+              System.out.println("oxygenTwox2: " + oxygenTwox2);
+              System.out.println("oxygenTwoy2: " + oxygenTwoy2);
+              System.out.println("rightHydrogenOnex1: " + rightHydrogenOnex1);
+              System.out.println("rightHydrogenOney1: " + rightHydrogenOney1);
+              System.out.println("rightHydrogenTwox2: " + rightHydrogenTwox2);
+              System.out.println("rightHydrogenTwoy2: " + rightHydrogenTwoy2);
+              
+              System.out.println("distance: " + distance);
+            
         try {
             // Create file 
-            FileWriter fstream = new FileWriter("output.txt");
+            FileWriter fstream = new FileWriter("newoutput.txt");
             BufferedWriter outputWriter = new BufferedWriter(fstream);
             //out.write("Test output");
             //out.write("Test: "+ Arrays.toString(moleculePositions));
-            moleculePositions = new double[4];
-            leftHydrogenOneCoord = new double[1];
-            rightHydrogenOneCoord = new double[1];
-            oxygenOneCoord = new double[1];
-            oxygenTwoCoord = new double[1];
-            leftHydrogenTwoCoord = new double[1];
-            rightHydrogenTwoCoord = new double[1];
-            
-            for(int n = 0; n < 4; n++) {
-                moleculePositions[n] = (double) (Math.random()*10);
-                arrayContentstoPrint = Arrays.toString(moleculePositions);
-            //sopl(Arrays.toString(moleculePositions));             
-            }
-            
+            //moleculePositions = new double[4];
+                 leftHydrogenOneCoord = new double[2];
+            rightHydrogenOneCoord = new double[2];
+            oxygenOneCoord = new double[2];
+            oxygenTwoCoord = new double[2];
+            leftHydrogenTwoCoord = new double[2];
+            rightHydrogenTwoCoord = new double[2];
+                 
             for(int i = 0; i < 2; i++) {
                 leftHydrogenOneCoord[i] = (double) (Math.random()*4);
                 rightHydrogenOneCoord[i] = (double) (Math.random()*4);
@@ -109,49 +123,36 @@ public class H2OInteractions {
                 rightHydrogenTwoCoord[i] = (double) (Math.random()*4);
             }
             
-            sopl(arrayContentstoPrint);
-            outputWriter.write("\n" + arrayContentstoPrint);
-            
-            //Close the output inputStream
+            outputWriter.write("The randomly generated molecules: \n\n");
+            outputWriter.write("leftHydrogenOneCoord: " + Arrays.toString(leftHydrogenOneCoord) + "\n");
+            outputWriter.write("rightHydrogenOneCoord: " + Arrays.toString(rightHydrogenOneCoord) + "\n");
+            outputWriter.write("oxygenOneCoord: " + Arrays.toString(oxygenOneCoord) + "\n");
+            outputWriter.write("oxygenTwoCoord: " + Arrays.toString(oxygenTwoCoord) + "\n");
+            outputWriter.write("leftHydrogenTwoCoord: " + Arrays.toString(leftHydrogenTwoCoord) + "\n");
+            outputWriter.write("rightHydrogenTwoCoord: " + Arrays.toString(rightHydrogenTwoCoord) + "\n");
             outputWriter.close();
         } 
         catch (Exception e){//Catch exception if any
             System.err.println("Error: " + e.getMessage());
         }
+        for(int i = 0; i < 2; i++) {
+        leftHydrogenOnex1 = leftHydrogenOneCoord[0];
+            leftHydrogenOney1 = leftHydrogenOneCoord[1];
+            leftHydrogenTwox2 = leftHydrogenTwoCoord[0];
+            leftHydrogenTwoy2 = leftHydrogenTwoCoord[1];
+            oxygenOnex1 = oxygenOneCoord[0];
+            oxygenOney1 = oxygenOneCoord[1];
+            oxygenTwox2 = oxygenTwoCoord[0];
+            oxygenTwoy2 = oxygenTwoCoord[1];
+            rightHydrogenOnex1 = rightHydrogenOneCoord[0];
+            rightHydrogenOney1 = rightHydrogenOneCoord[1];
+            rightHydrogenTwox2 = rightHydrogenTwoCoord[0];
+            rightHydrogenTwoy2 = rightHydrogenTwoCoord[1];
+        }
         
-        System.out.println(readFile("output.txt"));
-//        for(int i = 0; i < 2; i++) {
-//            x1 = moleculePositions[0];
-//            y1 = moleculePositions[1];
-//            x2 = moleculePositions[2];
-//            y2 = moleculePositions[3];
-//        }
-//            System.out.println(x1);
-//            System.out.println(y1);
-//            System.out.println(x2);
-//            System.out.println(y2);
-//            System.out.println("\nPoint Charge Calculation: " + calculatePointCharge(2));
-              leftHydrogenOnex1 = leftHydrogenOneCoord[0];
-              leftHydrogenOney1 = leftHydrogenOneCoord[1];
-              leftHydrogenTwox2 = leftHydrogenTwoCoord[0];
-              leftHydrogenTwoy2 = leftHydrogenTwoCoord[1];
-              oxygenOnex1 = oxygenOneCoord[0];
-              oxygenOney1 = oxygenOneCoord[1];
-              oxygenTwox2 = oxygenTwoCoord[0];
-              oxygenTwoy2 = oxygenTwoCoord[1];
-              rightHydrogenOnex1 = rightHydrogenOneCoord[0];
-              rightHydrogenOney1 = rightHydrogenOneCoord[1];
-              rightHydrogenTwox2 = rightHydrogenTwoCoord[0];
-              rightHydrogenTwoy2 = rightHydrogenTwoCoord[1];
-              
-              System.out.println(calculatePointCharge(oxygenOnex1, oxygenTwox2, oxygenOnex1, oxygenTwox2, distance));
-//              
-//            for(int i = 1; i < 10; i++) {
-//                distance = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
-//                energy = ((Constants.kConstant*hydrogenCharge*oxygenCharge)/Math.abs(distance));
-//                System.out.println(i+ " Test: "+ energy);
-//                
-//            }
+        System.out.println("This is my calculation: " + calculatePointCharge(leftHydrogenOnex1, leftHydrogenTwox2, leftHydrogenOney1, leftHydrogenTwoy2, distance));
+        System.out.println(readFile("newoutput.txt"));
+
     }
    
     private static String readFile(String path) throws IOException {
@@ -166,61 +167,16 @@ public class H2OInteractions {
             inputStream.close();
         }
     }
-    
-    private static void showRandomGeneratedPosition(int aStart, int aEnd, Random aRandom) {
-        if(aStart > aEnd) {
-            throw new IllegalArgumentException("Start cannot exceed End.");
-        }
-        //get the range, casting to long to avoid overflow problems
-        long range = (long)aEnd - (long)aStart + 1;
-        // compute a fraction of the range, 0 <= frac < range
-        long fraction = (long)(range * aRandom.nextDouble());
-        int randomNumber =  (int)(fraction + aStart);    
-        sopl("Generated : " + randomNumber);
-    }
   
     private static void sopl(String userInput){
         System.out.println(userInput);
     }
     
-    //Point (partial) charge calculation
-//    public static double calculatePointCharge(double distance) {
-//        for(int i = 1; i < 10; i++) {
-//            distance = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
-//            energy = ((Constants.kConstant*hydrogenCharge*oxygenCharge)/Math.abs(distance));
-//            System.out.println(energy + i);
-//        }
-////        energy += totalEnergy;
-////        return totalEnergy;
-//        return energy;
-//    }
-    
     public static double calculatePointCharge(double moleculeOneX1, double moleculeTwoX2, double moleculeOneY1, double moleculeTwoY2, double distance) {
-        distance = Math.sqrt(Math.sqrt(Math.pow(moleculeTwoX2-moleculeOneX1, 2) + Math.pow(moleculeTwoY2-moleculeOneY1, 2)));
-        energy = Constants.kConstant*hydrogenCharge*oxygenCharge/Math.abs(distance);
-        System.out.println(energy);
+        //        distance = Math.sqrt(Math.sqrt(Math.pow(moleculeTwoX2-moleculeOneX1, 2) + Math.pow(moleculeTwoY2-moleculeOneY1, 2)));
+//        energy = Constants.kConstant*hydrogenCharge*oxygenCharge/Math.abs(distance);
+//        System.out.println(energy);
+        energy = moleculeOneX1+moleculeOneY1+moleculeTwoX2+moleculeTwoY2;
         return energy;
-    }
-    
-//    
-//    public static double calculateDistance(double distance) {
-//        for(int i = 1; i < 10; i++) {
-//            distance = Math.sqrt((x2-x1)+(y2-y1));
-//        }
-//        distance += totalDistance;
-//        return totalDistance;
-//    }
-    
-//    public double calculateColumbForce(double momentOne, double momentTwo) {
-//        for(int i = 1; i < 10; i++) {
-//            energy = ((Constants.kConstant*momentOne*momentTwo)/totalDistance);
-//            energy+=totalEnergy;
-//        }
-//        return totalEnergy;
-//    }
-//    
-//    public static double calculateDistance(double xPosOne, double xPosTwo, double yPosOne, double yPosTwo, double tester) {
-//        totalDistance = Math.sqrt(((yPosTwo-yPosOne)*(yPosTwo-yPosOne))+((xPosTwo-xPosOne)*(xPosTwo-xPosOne)));
-//        return tester+=totalDistance;
-//    }   
+    }  
 }
