3678efc7da440a7d19d7b35fd8599fd7a7b2fbe1
Shawn Schwartz <shawn@sublimesynthesis.com>
2013-03-17 22:33:49 -0700

    Adds partial charge values to "constants.java" file for actual calculations and results.

--- H2OInteractions/src/h2ointeractions/Constants.java
+++ H2OInteractions/src/h2ointeractions/Constants.java
@@ -11,9 +11,9 @@ package h2ointeractions;
 
 public class Constants {
     public static final double kConstant = 8.9875517873681764;
-    public static final double hydrogenDipoleMoment = 1; //TODO: Find real value.
-    public static final double oxygenDipoleMoment = 1; //TODO: Find real value.
-    
-    public static final double KhydrogenPointCharge = 1; //TODO: Find real value.
-    public static final double KoxygenPointCharge = 1; //TODO: Find real value.
+//    public static final double hydrogenDipoleMoment = 1; //TODO: Find real value.
+//    public static final double oxygenDipoleMoment = 1; //TODO: Find real value.
+
+    public static final double KhydrogenPointCharge = 0.179; //TODO: Find real value.
+    public static final double KoxygenPointCharge = -0.358; //TODO: Find real value.
 }
